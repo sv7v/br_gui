9fb57583ed13d6863cf53b13d332482ed79043ca
sv7v <>
2024-03-24 18:38:51 +0300

    NEW Добавление класса читающего локальный текстовый файл

--- br_gui.py
+++ br_gui.py
@@ -14,3 +14,21 @@ class BG_Table:
 	def get(self):
 		return self._table
 #class BG_Table:
+
+class BG_LocalFile:
+	def __init__(self, inputId, func):
+		btn = document[inputId]
+
+		@bind(btn, 'input')
+		def file_read(ev):
+			def onload(event):
+				func(event.target.result)
+
+			# Get the selected file as a DOM File object
+			file = btn.files[0]
+			# Create a new DOM FileReader instance
+			reader = window.FileReader.new()
+			# Read the file content as text
+			reader.readAsText(file)
+			reader.bind('load', onload)
+#class BG_LocalFile:
